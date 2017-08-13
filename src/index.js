7e0a27c019d1f2b4806424b29dfc0cf1696dc4b6
Martin Lennervald <martin.lennervald@gmail.com>
2017-08-14 01:01:16 +0200

    Add express-handlebars with settings

--- src/index.js
+++ src/index.js
@@ -1,45 +1,52 @@
-require('dotenv').config();
 import http from 'http';
+import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import User from './model/user';
-const port = process.env.PORT || 3000;
+import hbs from 'express-handlebars';
+
+require('dotenv').config();
+
+import User from './model/user'
+import routes from './routes/index'
 
 let app = express();
 app.server = http.createServer(app);
 
-// Apply bodyParser to all http-requests
-// Set a limit as well in .env
-app.use(bodyParser.json({
-  limit: process.env.PARSER_LIMIT
-}));
+// view engine setup
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-// Connect to database
+
+// apply bodyParser, limit request size
+app.use(bodyParser.json({ limit: process.env.PARSER_LIMIT }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// init routes
+app.use('/', routes);
+
+// catch 404, forward to error handler
+app.use(function(req, res, next){
+  let err = new Error('404 Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// connect to database
 mongoose.connect(process.env.DB_URL, {
   useMongoClient: true
 });
 
-// When connected, run this
+// when connected, run this
 mongoose.connection.once('open', function(){
-
-});
-
-// Set view engine - hbs
-app.set('view engine', 'hbs');
-
-// Set express router
-let router = express.Router();
-
-// Add routes
-app.get('/test', function(req, res){
 
 });
 
 // Define static content
 app.use(express.static('public'));
 
-// Start server
-app.server.listen(port, function(){
-  console.log('Server running on port ' + port);
+// start server
+app.server.listen(process.env.PORT, function(){
+  console.log('Server running on port ' + process.env.PORT);
 });
